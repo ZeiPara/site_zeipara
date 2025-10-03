56e6d507ee8a7c700bbcf2dbdfbeb866d56b9b83
ZeiPara <kito20111011@outlook.jp>
2025-10-03 14:51:55 +0900

    Update script.js

--- audio/loop/script.js
+++ audio/loop/script.js
@@ -17,15 +17,18 @@ function startLoop() {
   stop.style.display = "block";
   audio.currentTime = 0;
   audio.play();
-  loopInterval = setInterval(() => {
-    audio.currentTime = 0;
-    audio.play();
-  }, 2000); // 2秒間隔で再生
+
+  audio.addEventListener('ended', loopHandler);
+}
+
+function loopHandler() {
+  audio.currentTime = 0;
+  audio.play();
 }
 
 function stopLoop() {
   start.style.display = "block";
   stop.style.display = "none";
-  clearInterval(loopInterval);
   audio.pause();
+  audio.removeEventListener('ended', loopHandler);
 }
